d5fde3bcfd2cd10fd40936f9a344baa3da56b360
snnaenu <455342265@qq.com>
2021-02-22 18:01:58 +0800

    first

--- examples/angular9/src/app/app.module.ts
+++ examples/angular9/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { APP_BASE_HREF } from '@angular/common';
  
  
 
@@ -12,6 +11,6 @@ import { AppComponent } from './app.component';
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule],
   bootstrap: [AppComponent],
-  providers: [{ provide: APP_BASE_HREF, useValue: '' }]
+  providers: []
 })
 export class AppModule {}
