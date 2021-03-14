ee20b2cdd9fe998f607a2b428055a181cf434df5
snnaenu <455342265@qq.com>
2021-03-15 00:31:22 +0800

    2

--- examples/vue/src/main.js
+++ examples/vue/src/main.js
@@ -1,30 +1,63 @@
-import './public-path';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import App from './App.vue';
-import routes from './router';
-import store from './store';
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import App from './App.vue'
+import MdPage from './md-page.vue'
+import { Message, MessageBox, Loading } from 'element-ui'
 
-Vue.config.productionTip = false;
-
-Vue.use(ElementUI);
-
+/**
+ * 期望功能：
+ * 1. 响应式布局
+ * 2. 左侧栏-文件目录
+ * 3. markdown(.md格式)文件渲染
+ * 4. 右侧栏-markdown文件标题定位
+ * 5. 向后滚动一段距离，头部隐藏
+ * 6. 左右侧栏滚动悬浮
+ */
+Vue.use(VueRouter)
+Vue.use(Loading.directive)
+Vue.prototype.$loading = Loading.service
+Vue.prototype.$message = Message
+Vue.prototype.$msgbox = MessageBox
 let router = null;
 let instance = null;
+const scrollBehavior = (to, from, savedPosition) => {
+	if (savedPosition) {
+		return savedPosition
+	}
+	const position = {}
+	if (to.hash) {
+		position.selector = to.hash
+	}
+
+	if (to.meta.scrollToTop !== false) {
+		position.x = 0
+		position.y = 0
+	}
+	return position
+}
+
+
+
 
 function render(props = {}) {
     const { container } = props;
     router = new VueRouter({
-        base: window.__POWERED_BY_QIANKUN__ ? '/vue' : '/',
-        mode: 'history',
-        routes,
-    });
-
+		mode: 'history',
+		base: window.__POWERED_BY_QIANKUN__ ? '/vue' : '/',
+		scrollBehavior,
+		routes: [
+			{
+				path: '/',
+				component: MdPage
+			},
+			{
+				path: '/acticles/:presentableId/:presentableName',
+				component: MdPage
+			}
+		]
+	})
     instance = new Vue({
         router,
-        store,
         render: h => h(App),
     }).$mount(container ? container.querySelector('#app') : '#app');
 }
@@ -33,28 +66,12 @@ if (!window.__POWERED_BY_QIANKUN__) {
     render();
 }
 
-function storeTest(props) {
-    props.onGlobalStateChange &&
-        props.onGlobalStateChange(
-            (value, prev) => console.log(`[onGlobalStateChange - ${props.name}]:`, value, prev),
-            true,
-        );
-    props.setGlobalState &&
-        props.setGlobalState({
-            ignore: props.name,
-            user: {
-                name: props.name,
-            },
-        });
-}
-console.log(location, document.getElementById('react15'))
 export async function bootstrap() {
     console.log('[vue] vue app bootstraped');
 }
 
 export async function mount(props) {
     console.log('[vue] props from main framework', props);
-    storeTest(props);
     render(props);
 }
 
