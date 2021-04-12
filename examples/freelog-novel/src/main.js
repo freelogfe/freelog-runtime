565eb366722d2dfd1ff8aa47e3b848d89cefa964
snnaenu <455342265@qq.com>
2021-04-12 19:38:48 +0800

    2

---
+++ examples/freelog-novel/src/main.js
@@ -0,0 +1,35 @@
+import "./public-path";
+import Vue from "vue";
+import router from "./router";
+import "./index.less";
+import App from "./pages/app.vue";
+
+let instance = null;
+
+function render(props = {}) {
+  const { container } = props;
+  instance = new Vue({
+    router,
+     render: (h) => h(App),
+  }).$mount(container ? container.querySelector("#app") : "#app");
+}
+
+if (!window.__POWERED_BY_FREELOG__) {
+  render();
+}
+
+export async function bootstrap() {
+  console.log("[vue] vue app bootstraped");
+}
+
+export async function mount(props) {
+  console.log("[vue] props from main framework", props);
+  render(props);
+}
+
+export async function unmount() {
+  instance.$destroy();
+  instance.$el.innerHTML = "";
+  instance = null;
+  router = null;
+}
