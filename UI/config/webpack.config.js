bb00f5368bd3ac2df7208a078bea021c5eb8f1ed
mac <mac@macdeMacBook-Pro.local>
2021-10-31 23:37:02 +0800

    ui

--- UI/config/webpack.config.js
+++ UI/config/webpack.config.js
@@ -131,22 +131,22 @@ module.exports = function (webpackEnv) {
               },
               stage: 3,
             }),
-          //   require('postcss-px-to-viewport')( {
-          //     unitToConvert: 'px', // 要转化的单位
-          //     viewportWidth: 375, // UI设计稿的宽度
-          //     unitPrecision: 5,  // 转换后的精度，即小数点位数
-          //     propList: ['*'],   // 指定转换的css属性的单位，*代表全部css属性的单位都进行转换
-          //     viewportUnit: 'vw', // 指定需要转换成的视窗单位，默认vw
-          //     fontViewportUnit: 'vw', // 指定字体需要转换成的视窗单位，默认vw
-          //     selectorBlackList: [], // 指定不转换为视窗单位的类名，
-          //     minPixelValue: 1, // 默认值1，小于或等于1px则不进行转换
-          //     mediaQuery: false, // 是否在媒体查询的css代码中也进行转换，默认false
-          //     replace: true,    // 是否转换后直接更换属性值
-          //     exclude: [/node_modules\\antd$/,/src\\assets\\css/,/src\\ui\\pc/],     // 设置忽略文件，用正则做目录名匹配
-          //     landscape: false, // 是否处理横屏情况
-          //     landscapeUnit: 'vw',  // 横屏时使用的单位
-          //     landscapeWidth: 568  // 用于横向定向的视口宽度。
-          // }),
+            require('postcss-px-to-viewport')( {
+              unitToConvert: 'px', // 要转化的单位
+              viewportWidth: 375, // UI设计稿的宽度
+              unitPrecision: 5,  // 转换后的精度，即小数点位数
+              propList: ['*'],   // 指定转换的css属性的单位，*代表全部css属性的单位都进行转换
+              viewportUnit: 'vw', // 指定需要转换成的视窗单位，默认vw
+              fontViewportUnit: 'vw', // 指定字体需要转换成的视窗单位，默认vw
+              selectorBlackList: [], // 指定不转换为视窗单位的类名，
+              minPixelValue: 1, // 默认值1，小于或等于1px则不进行转换
+              mediaQuery: false, // 是否在媒体查询的css代码中也进行转换，默认false
+              replace: true,    // 是否转换后直接更换属性值
+              include: [/node_modules\\antd-mobile$/,/src\\assets\\css\\mobile/,/src\\ui\\mobile/],
+              landscape: false, // 是否处理横屏情况
+              landscapeUnit: 'vw',  // 横屏时使用的单位
+              landscapeWidth: 568  // 用于横向定向的视口宽度。
+          }),
             // Adds PostCSS Normalize as the reset css with default options,
             // so that it honors browserslist config in package.json
             // which in turn let's users customize the target behavior as per their needs.
