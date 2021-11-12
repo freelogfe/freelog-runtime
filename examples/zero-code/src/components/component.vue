f6b0ef4af367cba70de793a121d612f7b163c458
mac <mac@macdeMacBook-Pro.local>
2021-11-13 05:29:33 +0800

    ui

---
+++ examples/zero-code/src/components/component.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <component :is="comp" v-bind='compProps'></component>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      comp: 'HelloWorld',
+      compProps: {
+        msg: 'Welcome to Your Vue.js + TypeScript App'
+      }
+    }
+  }
+})
+</script>
---
+++ examples/zero-code/src/components/container.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <component :is="comp" v-bind='compProps'></component>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+/**
+ * 负责循环传递组件配置给子组件，同时路由也在这里写。
+ * 
+ */
+export default defineComponent({
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      comp: 'HelloWorld',
+      compProps: {
+        msg: 'Welcome to Your Vue.js + TypeScript App'
+      }
+    }
+  }
+})
+</script>
