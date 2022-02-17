141f79ae8bf15ae85be80641d572da86445f65c3
snnaenu <455342265@qq.com>
2022-02-17 18:00:26 +0800

    ui

--- UI/pc/src/views/event/pay.tsx
+++ UI/pc/src/views/event/pay.tsx
@@ -22,8 +22,12 @@ interface PayProps {
 }
 
 export default function Pay(props: PayProps) {
-  const [password, setPassword] = useState("");
+  const [passwords, setPasswords] = useState<any>(["", "", "", "", "", ""]);
   const [loading, setLoading] = useState(false);
+  // 1: 支付中  2: 支付成功  3: 密码错误   4: 支付失败：需要考虑网络超时
+  const [tipType, setTipType] = useState(0);
+  const [focus, setFocus] = useState(0);
+
   const [visible, setVisible] = useState(false);
   const [isTipVisible, setIsTipVisible] = useState(false);
   const [tipConfig, setTipConfig] = useState({
@@ -33,6 +37,13 @@ export default function Pay(props: PayProps) {
   });
   const inputP = useRef(null);
   const [userAccount, setUserAccount] = useState<any>({});
+  const input1 = useRef(null);
+  const input2 = useRef(null);
+  const input3 = useRef(null);
+  const input4 = useRef(null);
+  const input5 = useRef(null);
+  const input0 = useRef(null);
+  const inputs = [input0, input1, input2, input3, input4, input5];
   const handleOk = () => {
     props.setIsModalVisible(false);
   };
@@ -52,14 +63,19 @@ export default function Pay(props: PayProps) {
     }, 200);
   }
   useEffect(() => {
-    setPassword("");
+    setPasswords(["", "", "", "", "", ""]);
+    setTimeout(() => {
+      // @ts-ignore
+      input0.current.focus();
+    }, 100);
     setVisible(props.isModalVisible);
 
     props.isModalVisible && getAccount();
   }, [props.isModalVisible]);
-  async function pay() {
+  async function pay(password: any) {
     // TODO 防止多次点击
     if (loading) return;
+    setTipType(1);
     setLoading(true);
     const payResult = await frequest(event.pay, [props.contractId], {
       eventId: props.eventId,
@@ -67,17 +83,33 @@ export default function Pay(props: PayProps) {
       transactionAmount: props.transactionAmount,
       password: password,
     });
-    setIsTipVisible(true);
+    // 这里考虑支付超时
     if (payResult.data.errCode !== 0) {
+      if (payResult.data.data && payResult.data.data.code === "E1010") {
+        setTipType(3);
+        setPasswords(["", "", "", "", "", ""]);
+        setLoading(false);
+        setTimeout(() => {
+          // @ts-ignore
+          input0.current.focus();
+        }, 100);
+        return;
+      }
       setTipConfig({
         content: payResult.data.msg,
         type: "error",
         mask: false,
       });
-      setLoading(false);
+      setTimeout(() => {
+        setLoading(false);
+        setTimeout(() => {
+          // @ts-ignore
+          input5.current.focus();
+        }, 100);
+      }, 1500);
       return;
     }
-
+    setTipType(2);
     setTipConfig({
       content: "支付成功, 系统处理中...",
       type: "success",
@@ -92,19 +124,63 @@ export default function Pay(props: PayProps) {
       );
       const status = res.data.data.status;
       if ([2, 3, 4].includes(status)) {
-        setLoading(false);
-        setIsTipVisible(false);
-        setTimeout(() => props.paymentFinish(status), 100);
         window.clearInterval(flag);
+        setIsTipVisible(false);
+        setLoading(false);
+        setTimeout(() => {
+          props.paymentFinish(status);
+        }, 10);
       }
     }, 2000);
-
-    // setLoading(false);
-    //   eventId: "string",  contractId
-    //   accountId: "int",
-    //   transactionAmount: "string",
-    //   password: "string"
   }
+  // async function pay(pass: any) {
+  //   // TODO 防止多次点击
+  //   if (loading) return;
+  //   setLoading(true);
+  //   const payResult = await frequest(event.pay, [props.contractId], {
+  //     eventId: props.eventId,
+  //     accountId: userAccount.accountId,
+  //     transactionAmount: props.transactionAmount,
+  //     password: pass,
+  //   });
+  //   setIsTipVisible(true);
+  //   if (payResult.data.errCode !== 0) {
+  //     setTipConfig({
+  //       content: payResult.data.msg,
+  //       type: "error",
+  //       mask: false,
+  //     });
+  //     setLoading(false);
+  //     return;
+  //   }
+
+  //   setTipConfig({
+  //     content: "支付成功, 系统处理中...",
+  //     type: "success",
+  //     mask: true,
+  //   });
+  //   // TODO 查交易状态, flag应该设为状态，在关闭弹窗时清除
+  //   const flag = setInterval(async () => {
+  //     const res: any = await frequest(
+  //       transaction.getRecord,
+  //       [payResult.data.data.transactionRecordId],
+  //       ""
+  //     );
+  //     const status = res.data.data.status;
+  //     if ([2, 3, 4].includes(status)) {
+  //       setLoading(false);
+  //       setIsTipVisible(false);
+  //       setTimeout(() => props.paymentFinish(status), 100);
+  //       window.clearInterval(flag);
+  //     }
+  //   }, 2000);
+
+  //   // setLoading(false);
+  //   //   eventId: "string",  contractId
+  //   //   accountId: "int",
+  //   //   transactionAmount: "string",
+  //   //   password: "string"
+  // }
   return (
     <Modal
       title="支付"
@@ -148,15 +224,74 @@ export default function Pay(props: PayProps) {
             </div>
           </div>
         </div>
-        <div
-          className="forgot-p text-align-right px-80 mt-18 cur-pointer"
-          onClick={() => {
-            window.open("http://user.testfreelog.com/retrievePayPassword");
-          }}
-        >
-          忘记密码
+        <div className="text-center mt-40">
+          <div className="enter-tip mb-20">输入支付密码进行支付</div>
+          <div className={"" + (tipType === 3 ? "password-error-input" : "")}>
+            {[0, 0, 0, 0, 0, 0].map((item: any, index: any) => {
+              return (
+                <input
+                  type="password"
+                  maxLength={1}
+                  minLength={1}
+                  key={index}
+                  ref={inputs[index]}
+                  value={passwords[index]}
+                  onChange={(e: any) => {}}
+                  onClick={(e: any) => {
+                    // @ts-ignore
+                    inputs[focus].current.focus();
+                  }}
+                  onKeyDown={(e: any) => {
+                    setTipType(0);
+                    // alert(e.keyCode)
+                    const p = [...passwords];
+                    if (
+                      [46, 8, 229].includes(e.keyCode) &&
+                      index > 0 &&
+                      isNaN(parseInt(p[index]))
+                    ) {
+                      // @ts-ignore
+                      if (inputs[index - 1]) {
+                        // @ts-ignore
+                        inputs[index - 1].current.focus();
+                        setFocus(index - 1);
+                      }
+                      p[index - 1] = "";
+                      setPasswords(p);
+                      return;
+                    }
+                    if (!isNaN(parseInt(e.key))) {
+                      p[index] = e.key;
+
+                      if (inputs[index + 1]) {
+                        // @ts-ignore
+                        inputs[index + 1].current.focus();
+                        setFocus(index + 1);
+                      }
+                      if (index === 5) {
+                        const password = p.join("");
+                        pay(password);
+                      }
+                    } else {
+                      p[index] = "";
+                    }
+                    setPasswords(p);
+                  }}
+                />
+              );
+            })}
+          </div>
+          <div
+            className="forgot-p cur-pointer"
+            onClick={() => {
+              window.open("http://user.testfreelog.com/retrievePayPassword");
+            }}
+          >
+            忘记密码
+          </div>
         </div>
-        <div className="px-80 pt-5">
+
+        {/* <div className="px-80 pt-5">
           <Input.Password
             size="large"
             ref={inputP}
@@ -167,8 +302,8 @@ export default function Pay(props: PayProps) {
             value={password}
             placeholder="输入6位支付密码"
           />
-        </div>
-        <div className="px-80 pt-20">
+        </div> */}
+        {/* <div className="px-80 pt-20">
           <Button
             click={pay}
             disabled={password.length !== 6 || loading}
@@ -183,7 +318,7 @@ export default function Pay(props: PayProps) {
               "确认支付"
             )}
           </Button>
-        </div>
+        </div> */}
       </div>
     </Modal>
   );
