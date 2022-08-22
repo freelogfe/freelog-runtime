95c185e0b18f5d4420bfc70984308f6640a17238
snnaenu <455342265@qq.com>
2022-08-22 09:57:02 +0800

    docs

--- packages/examples/vue3/src/views/Home.vue
+++ packages/examples/vue3/src/views/Home.vue
@@ -1,35 +1,55 @@
 <template>
-  <img alt="Vue logo" src="../assets/logo.png" />
+  <img alt="Vue logo" src="../assets/logo.png" id="img"/>
   <button @click="reload">reload</button>
   <button @click="show">show window</button>
-  <HelloWorld msg="Vue.js Demo" @close="close"> </HelloWorld>
+
+  <button @click="clip">剪切图片</button>
+  <HelloWorld msg="Vue.js Demo" @close="close" > </HelloWorld>
+  <table id="table">
+    <th>2222</th>
+    <tr><td>232323</td> <td>232323</td><td>232323</td><td>232323</td><td>232323</td><td>232323</td></tr>
+     <tr><td>232323</td> <td>232323</td><td>232323</td><td>232323</td><td>232323</td><td>232323</td></tr>
+
+    <tr><td>232323</td> <td>232323</td><td>232323</td><td>232323</td><td>232323</td><td>232323</td></tr>
+
+    <tr><td>232323</td> <td>232323</td><td>232323</td><td>232323</td><td>232323</td><td>232323</td></tr>
+
+    <tr><td>232323</td> <td>232323</td><td>232323</td><td>232323</td><td>232323</td><td>232323</td></tr>
+
+  </table>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue';
+import HelloWorld from "@/components/HelloWorld.vue";
+import html2canvas from "html2canvas";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld,
   },
   created() {
-    console.log('vue2.0写法 created');
+    console.log("vue2.0写法 created");
   },
   mounted() {
     console.log(this.$route);
   },
   methods: {
+    clip() {
+      html2canvas(document.getElementById('table')).then(function (canvas) {
+        document.body.appendChild(canvas);
+      });
+    },
     reload() {
-      window.data = '23223232323'
-      window.location.reload()
+      window.data = "23223232323";
+      window.location.reload();
     },
-    show(){
-      console.log(window.data)
+    show() {
+      console.log(window.data);
     },
-    
+
     close() {
-      console.log('close');
+      console.log("close");
     },
   },
 };
