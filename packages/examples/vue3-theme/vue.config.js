eaaef81fa29169bfabfba9fd7340a1339dbfec8f
snnaenu <455342265@qq.com>
2023-06-13 17:08:49 +0800

    https 问题

--- packages/examples/vue3-theme/vue.config.js
+++ packages/examples/vue3-theme/vue.config.js
@@ -1,35 +1,81 @@
+const { defineConfig } = require('@vue/cli-service')
+
 const path = require('path');
 const { name } = require('./package');
+const webpackPlugin = require('webpack-mkcert')
 
 function resolve(dir) {
   return path.join(__dirname, dir);
 }
 
 const port = 8001;
-
-module.exports = {
-  outputDir: 'dist',
-  assetsDir: 'static',
-  filenameHashing: true,
-  devServer: {
-    hot: true,
-    port,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-  },
-  // 自定义webpack配置
-  configureWebpack: {
-    resolve: {
-      alias: {
-        '@': resolve('src'),
+module.exports = defineConfig(async () => {
+  const https = await webpackPlugin.default({
+    source: 'coding',
+    hosts: ['localhost', '127.0.0.1']
+  })
+  console.log(https)
+  return {
+    transpileDependencies: true,
+    outputDir: 'dist',
+    assetsDir: 'static',
+    filenameHashing: true,
+    devServer: {
+      https: {
+        // ca: './path/to/server.pem',
+        // pfx: './path/to/server.pfx',
+        // key: './path/to/server.key',
+        // cert: './path/to/server.crt',
+        // passphrase: 'webpack-dev-server',
+        // requestCert: true,
+        ...https
+      },
+      hot: true,
+      port,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
       },
     },
-    output: {
-      // 把子应用打包成 umd 库格式
-      library: `${name}-[name]`,
-      libraryTarget: 'umd',
-      chunkLoadingGlobal: `webpackJsonp_${name}`,
+    // 自定义webpack配置
+    configureWebpack: {
+      resolve: {
+        alias: {
+          '@': resolve('src'),
+        },
+      },
+      output: {
+        // 把子应用打包成 umd 库格式
+        library: `${name}-[name]`,
+        libraryTarget: 'umd',
+        chunkLoadingGlobal: `webpackJsonp_${name}`,
+      },
     },
-  },
-};
+  }
+})
+// module.exports = {
+//   outputDir: 'dist',
+//   assetsDir: 'static',
+//   filenameHashing: true,
+//   devServer: {
+//     hot: true,
+//     port,
+//     headers: {
+//       'Access-Control-Allow-Origin': '*',
+//     },
+//     ...https
+//   },
+//   // 自定义webpack配置
+//   configureWebpack: {
+//     resolve: {
+//       alias: {
+//         '@': resolve('src'),
+//       },
+//     },
+//     output: {
+//       // 把子应用打包成 umd 库格式
+//       library: `${name}-[name]`,
+//       libraryTarget: 'umd',
+//       chunkLoadingGlobal: `webpackJsonp_${name}`,
+//     },
+//   },
+// };
