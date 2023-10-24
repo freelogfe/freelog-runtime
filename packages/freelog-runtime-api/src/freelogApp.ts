3f418f724dd5973f95d36c7760d7eea2f3deb93d
snnaenu <455342265@qq.com>
2023-10-24 18:00:01 +0800

    开始分离后端api

---
+++ packages/freelog-runtime-api/src/freelogApp.ts
@@ -0,0 +1,33 @@
+﻿// 需要导出 user.getCurrent   node.putUserData  node.getUserData  三个
+
+import {
+  getExhibitListById,
+  getExhibitListByPaging,
+  getExhibitInfo,
+  getExhibitSignCount,
+  getExhibitAuthStatus,
+  getExhibitFileStream,
+  getExhibitDepFileStream,
+  getExhibitInfoByAuth,
+  getExhibitDepInfo,
+  getExhibitDepTree,
+  getSignStatistics,
+  getExhibitAvailalbe,
+  pushMessage4Task,
+} from "./api";
+export { init } from "./api";
+export const freelogApp: any = {
+  pushMessage4Task,
+  getExhibitListById,
+  getExhibitListByPaging,
+  getExhibitInfo,
+  getExhibitSignCount,
+  getExhibitAuthStatus,
+  getExhibitFileStream,
+  getExhibitDepFileStream,
+  getExhibitInfoByAuth,
+  getExhibitDepInfo,
+  getExhibitDepTree,
+  getSignStatistics,
+  getExhibitAvailalbe,
+};
---
+++ packages/freelog-runtime-api/src/index.ts
@@ -0,0 +1,1 @@
+export { freelogApp, init } from "./freelogApp";
